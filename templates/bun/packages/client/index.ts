7864620862224eb0161f389161aa278bf2977725
akirarika <33272184+akirarika@users.noreply.github.com>
2024-05-10 15:40:27 +0800

    ✨ feat: update

--- templates/bun/packages/client/index.ts
+++ templates/bun/packages/client/index.ts
@@ -1,11 +1,10 @@
-export * from "./types"
-import { failCode } from "./project/src/fail-code"
-import type ApiSchema from "./project/generated/api-schema"
-export { defineMiddleware } from "milkio-client"
-import { defineMiddleware, defineMilkioClient } from "milkio-client"
+export * from "./types";
+import { failCode } from "./project/src/fail-code";
+import type ApiSchema from "./project/generated/api-schema";
+import { defineMiddleware, defineMilkioClient } from "milkio-client";
 
 export const createClient = defineMilkioClient<typeof ApiSchema, typeof failCode>([
-  //
-])
+	//
+]);
 
-export const FailCode = failCode
+export const FailCode = failCode;
