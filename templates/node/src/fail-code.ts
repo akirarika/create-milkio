023ca93c75bfad9232573c5fa77f415624845f38
conglinyizhi <cslbyivi@outlook.com>
2024-07-20 14:26:06 +0800

    fix(type): 对不使用的变量添加前置下划线

--- templates/node/src/fail-code.ts
+++ templates/node/src/fail-code.ts
@@ -1,10 +1,10 @@
 import type { MilkioFailCode } from "milkio";
 
 export const failCode = {
-	NETWORK_ERROR: (data: undefined) => "Network Error",
-	INTERNAL_SERVER_ERROR: (data: undefined) => "Internal Server Error",
-	NOT_FOUND: (data: undefined) => "Not Found",
-	NOT_ALLOW_METHOD: (data: undefined) => "Not Allow Method",
+	NETWORK_ERROR: (_data: undefined) => "Network Error",
+	INTERNAL_SERVER_ERROR: (_data: undefined) => "Internal Server Error",
+	NOT_FOUND: (_data: undefined) => "Not Found",
+	NOT_ALLOW_METHOD: (_data: undefined) => "Not Allow Method",
 	TYPE_SAFE_ERROR: (data: { path: string; expected: string; value: string }) => `Parameter Error: The current value is '${data.value}', which does not meet '${data.expected}' requirements`,
 	BUSINESS_FAIL: (data: string) => `${data}`,
 	// You can add your own mistakes here
